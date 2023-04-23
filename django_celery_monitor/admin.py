4ac64521f22a4ac9e2bb0a27523e6e53e0cad271
Norberto Gomez <norberto.gomez@mitratech.com>
2023-04-22 23:59:02 -0600

    Fix python2 syntax

--- django_celery_monitor/admin.py
+++ django_celery_monitor/admin.py
@@ -8,7 +8,7 @@ from django.contrib.admin import helpers
 from django.contrib.admin.views import main as main_views
 from django.shortcuts import render
 from django.template import RequestContext
-from django.utils.encoding import force_text
+from django.utils.encoding import force_str
 from django.utils.html import escape
 from django.utils.translation import gettext_lazy as _
 
@@ -205,7 +205,7 @@ class TaskMonitor(ModelMonitor):
         context = {
             'title': _('Rate limit selection'),
             'queryset': queryset,
-            'object_name': force_text(opts.verbose_name),
+            'object_name': force_str(opts.verbose_name),
             'action_checkbox_name': helpers.ACTION_CHECKBOX_NAME,
             'opts': opts,
             'app_label': app_label,
